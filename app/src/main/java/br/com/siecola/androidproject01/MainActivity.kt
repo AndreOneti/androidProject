338ad3df6bf15556ee0a581b6a320365123354b8
AndreOneti <andreluiz.onete@hotmail.com>
2020-07-04 15:41:39 -0300

    :zap: Add button function
    Create button on click

--- app/src/main/java/br/com/siecola/androidproject01/MainActivity.kt
+++ app/src/main/java/br/com/siecola/androidproject01/MainActivity.kt
@@ -2,11 +2,30 @@ package br.com.siecola.androidproject01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import br.com.siecola.androidproject01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnSave.setOnClickListener {
+            Log.d("MainActivity","Save button pressed")
+
+            if(binding.edtName.text.isNotEmpty()){
+                binding.txtDescription.text = binding.edtDescription.text
+                binding.txtPrice.text = binding.edtPrice.text
+                binding.txtName.text = binding.edtName.text
+                binding.txtCode.text = binding.edtCode.text
+            }else{
+                Toast.makeText(this, "Please, enter the name.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
